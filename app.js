56b1243656f48def701d1bc2253a49d200b99dc4
Robert Nguyen <robert@gotitapp.co>
2021-01-12 22:19:28 +0700

    feat: first challenge - print out weather forecast

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=3e37e0465f29b58118c48c9d06609fd1&query=37.8267,-122.4233'
+const url = 'http://api.weatherstack.com/current?access_key=3e37e0465f29b58118c48c9d06609fd1&query=37.8267,-122.4233&units=f'
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body)
-  console.log(data.current)
+request({ 
+  url: url,
+  json: true
+ }, (error, response) => {
+  const current = response.body.current
+  const { temperature, feelslike, weather_descriptions } = current;
+  console.log(`${weather_descriptions?.[0] || 'Nothing special'}. It is currently ${temperature} but it feels like ${feelslike} out there`)
 })
